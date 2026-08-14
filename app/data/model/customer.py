68a85bb72447f69235a3b40da7ffa0f1510fe872
JoonHyuk0331 <joonlife0901@gmail.com>
2026-08-15 08:00:12 +0900

    fix: date 관련 import 오류 수정

--- app/data/model/customer.py
+++ app/data/model/customer.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import date, datetime
 
-from sqlalchemy import CheckConstraint, Column, DateTime, SmallInteger
+from sqlalchemy import CheckConstraint, Column, Date, DateTime, SmallInteger
 from sqlmodel import Field, SQLModel
 
 
@@ -36,8 +36,6 @@ class Customer(SQLModel, table=True):
     )
     credit_rating: int = Field(sa_column=Column(SmallInteger, nullable=False))
     loan_type: str = Field(max_length=8)
-    email: str | None = Field(default=None, max_length=255)
-    phone_number: str | None = Field(default=None, max_length=32)
     created_at: datetime = Field(
         default_factory=datetime.now,
         sa_column=Column(DateTime(timezone=True), nullable=False),
